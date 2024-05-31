b06fd3a99a32de98fae618d2aca32f1f55a91fde
ramazonLider <166911849+ramazonLider@users.noreply.github.com>
2024-05-31 15:35:04 +0500

    bhjb

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+import "@/styles/static.css";
+import "@/styles/css.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
