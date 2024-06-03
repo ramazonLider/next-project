f98fbb4986c21eedb32886854694c9f5bfef617d
ramazonLider <166911849+ramazonLider@users.noreply.github.com>
2024-06-03 13:47:46 +0500

    kk

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,30 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/polyfills-c67a75d1b6f99dc8.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/6198.3b45aead03971f46.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/9974.85b39740f0b672a2.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/webpack-510ed341bb4f8d50.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/framework-1f1fb5c07f2be279.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/pages/_app-58a9e918f4060e69.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/main-b13012f43f3493ad.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/3675-295a9ea20de1cab0.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/4567-e5af9e87946df0c7.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/2141-ea4a2a75e9026049.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/9755-637772a1f5c6dedc.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/9331-91f008607e11c87c.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/8279-2ad6f7c3cf810b0c.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/2063-39d9b4c356fabdaf.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/4504-741046cfc4149f1f.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/3968-c72362f16aec3932.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/7649-59f4c9faaac91be7.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/5854-18c6cf6523c2f3fa.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/1817-d39846485a7a0214.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/chunks/pages/car-finder-ef6a3d2a81337a9c.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/PIFHlHCVYrb9vwpZPP46D/_buildManifest.js" async></script>
+          <script src="https://finder-react.createx.studio/_next/static/PIFHlHCVYrb9vwpZPP46D/_ssgManifest.js" async></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
